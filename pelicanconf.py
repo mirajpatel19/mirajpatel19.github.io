10b6a6e87f4f91f41ed37bb9251ae381de02eeb7
Miraj Patel <mirajpatel1995@gmail.com>
2019-03-11 15:58:10 -0700

    New publication

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,7 +19,6 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-#___________________________________________________
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
@@ -29,8 +28,6 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
-#___________________________________________________
-
 
 DEFAULT_PAGINATION = 10
 
